9f519888ef024841aa5f001cc1dea406e13b1928
SergioDF07 <sadiazf@ufpso.edu.co>
2026-09-18 10:09:06 -0500

    drawer terminado (?)

--- lib/presentation/shared/drawer_custom.dart
+++ lib/presentation/shared/drawer_custom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/presentation/home/screens/home_screen.dart';
 import 'package:mobile_app/presentation/user1/user1.dart';
 import 'package:mobile_app/presentation/user2/user2.dart';
 
@@ -15,51 +16,29 @@ class DrawerCustom extends StatelessWidget {
               padding: EdgeInsets.zero,
               children: [
                 DrawerHeader(
-                  decoration: const BoxDecoration(color: Colors.redAccent),
+                  decoration: const BoxDecoration(color: Color(0xFF6C7D8A)),
                   child: Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const CircleAvatar(
-                          radius: 32,
-                          backgroundImage: NetworkImage(
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA5XM2PsMVORi1kfQ4ExQpO09vlPkzrMQqwM7SIIq9PA&s=10",
-                          ),
-                        ),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                "My name",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16.0,
-                                  color: Colors.indigoAccent,
-                                ),
-                              ),
-                              Text(
-                                "My title",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12.0,
-                                  color: Colors.indigoAccent,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                    alignment: Alignment.centerLeft,
+                    child: ListTile(
+                      title: const Text("Midterm exam"),
+                      subtitle: const Text("wroguhhf"),
+                    )
                   ),
                 ),
                 ListTile(
-                  title: const Text("Direction 1"),
-                  subtitle: const Text("This is a description about the page"),
-                  trailing: const Icon(Icons.arrow_forward),
+                  title: const Text("Home"),
+                  subtitle: const Text("Main screens"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomeScreen()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: const Text("Jose Angarita"),
+                  subtitle: const Text("General personal information about Jose"),
                   onTap: () {
                     Navigator.pop(context);
                     Navigator.push(
@@ -69,9 +48,8 @@ class DrawerCustom extends StatelessWidget {
                   },
                 ),
                 ListTile(
-                  title: const Text("Direction 2"),
-                  subtitle: const Text("This is a description about the page"),
-                  trailing: const Icon(Icons.arrow_forward),
+                  title: const Text("Sergio Diaz"),
+                  subtitle: const Text("General personal information about Sergio"),
                   onTap: () {
                     Navigator.pop(context);
                     Navigator.push(
